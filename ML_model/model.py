a088f57fa6c33beb7d10b68ac0913eb699bbf8fc
Francisco J. Londono <69941126+franciscoj-londonoh@users.noreply.github.com>
2021-03-14 00:12:14 -0500

    Add files via upload

---
+++ ML_model/model.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+data = pd.read_csv("data.csv")
+
+X = data.drop(columns=['artists', 'name', 'id', 'release_date', 'mode', 'explicit',  'key'])
+y = data['artists']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
+
+X_train1 = X_train[0:10000]
+y_train1 = y_train[0:10000]
+
+clf = RandomForestClassifier(n_estimators=10)
+clf = clf.fit(X_train1, y_train1)
+
+pickle.dump(clf, open('model.pkl', 'wb'))
